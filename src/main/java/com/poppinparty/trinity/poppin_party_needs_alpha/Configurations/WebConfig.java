c138a065af47b6c257ecb921d51462b90fb7ec1b
novakamiii <pauloneil3334@gmail.com>
2025-06-24 13:42:45 +0800

    Updated the account management and product managements. Included a new crop API.

--- src/main/java/com/poppinparty/trinity/poppin_party_needs_alpha/Configurations/WebConfig.java
+++ src/main/java/com/poppinparty/trinity/poppin_party_needs_alpha/Configurations/WebConfig.java
@@ -1,12 +1,3 @@
-/**
- * WebConfig is a Spring configuration class that customizes the handling of static resources.
- * <p>
- * This configuration maps all requests with the pattern "/uploads/**" to the local file system directory "uploads/".
- * Files placed in the "uploads" directory will be accessible via HTTP requests to "/uploads/{filename}".
- * </p>
- *
- * Implements {@link WebMvcConfigurer} to override the {@code addResourceHandlers} method.
- */
 package com.poppinparty.trinity.poppin_party_needs_alpha.Configurations;
 
 import org.springframework.context.annotation.Configuration;
